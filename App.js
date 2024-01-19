5b42180273129edcf215cbd793feadf050076693
c2toto00 <c2toto00@students.oamk.fi>
2024-01-19 07:36:44 +0200

    modal added

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
+import React, { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    const [modalVisible, setModalVisible] = useState(false);
+    return (
+        <View style={styles.container}>
+            <Modal
+                style={styles.modal}
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {setModalVisible(!modalVisible)}}
+            >
+                <View style={[styles.container, styles.modal]}>
+                    <View style={styles.container}>
+                        <Text style={styles.modalText}>This is modal</Text>
+                        <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                            <Text style={styles.text}>Hide modal message</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Pressable onPress={() => setModalVisible(true)}>
+                <Text style={styles.text}>Show modal message</Text>
+            </Pressable>
+        </View>
+    );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +37,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    borderWidth: 1,
+    height: 50,
+    width: 200,
+    borderColor: "black",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+  modal: {
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  modalText: {
+    paddingBottom: 20,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
 });
+
+export default App;
